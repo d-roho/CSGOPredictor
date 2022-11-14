46cf9a6b0dc20965f1b4d730c30de766f4174481
d-roho <52367637+d-roho@users.noreply.github.com>
2022-11-15 00:09:12 +0530

    Create exceptions.py for exceptions handling and some Pause-and-Play functionality

---
+++ exceptions.py
@@ -0,0 +1,18 @@
+#List of Custom Exceptions used to capture known potential errors
+
+
+class Error(Exception):
+#Base class for other exceptions
+    pass
+
+class EmptyServer(Error):
+#Raised when server has a population of zero
+    pass
+
+class MatchNotStarted(Error):
+	#Raised when user is not currently spectating a Match (usually occurs when they've just exited out of a match)
+	pass
+
+class WarmUp(Error):
+	#Raised when match is in "Warm Up" phase (usually occurs when user changes from one match to another (which is in Warm Up phase))
+	pass
